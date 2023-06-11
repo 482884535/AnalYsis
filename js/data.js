aed8392c3d2d44e3ca1a477caa5b4aeb321cb7e1
482884535 <76425856+482884535@users.noreply.github.com>
2023-06-11 21:27:22 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,12 @@
+const routes = [
+  {name: '解析线路1', url: 'https://svip.bljiex.cc/?v='}, 
+  {name: '解析线路2', url: 'https://jx.xmflv.com/?url='},
+  {name: '解析线路3', url: 'https://jx.jsonplayer.com/player/?url='},
+  {name: '', url: ''},
+  {name: '', url: ''}, 
+]
+
+const friends = [
+  {name: '待添加', url: '#'},
+  {name: '待添加', url: '#'},
+]
